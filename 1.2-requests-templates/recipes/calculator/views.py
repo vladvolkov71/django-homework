1d15c233c41745cacf9757f7523d80fdb1a5c864
“Vlad <vladvolkov71@gmail.com>
2024-03-28 14:43:35 +0300

    Решение задания 2 Рецепты

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -1,30 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
+
+
+def home_view(request):
+    template_name = 'calculator/index.html'
+    # впишите правильные адреса страниц, используя
+    # функцию `reverse`
+    pages = {
+        'Главная страница': reverse('home'),
+        'РЕЦЕПТЫ': reverse('recipes'),
+        #'КАЛЬКУЛЯТОР': reverse('calculator'),
+    }
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
 
 DATA = {
-    'omlet': {
+    'Омлет': {
         'яйца, шт': 2,
         'молоко, л': 0.1,
         'соль, ч.л.': 0.5,
     },
-    'pasta': {
+    'Паста': {
         'макароны, г': 0.3,
         'сыр, г': 0.05,
     },
-    'buter': {
+    'Бутерброд': {
         'хлеб, ломтик': 1,
         'колбаса, ломтик': 1,
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
     },
-    # можете добавить свои рецепты ;)
-}
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+}
+def recipes_view(request):
+    template_name = 'calculator/recipes.html'
+    recipes = DATA.keys()
+    context = {
+        'recipes': recipes
+    }
+    return render(request, template_name, context)
+
+def recipe_view(request, recipe):
+    template_name = 'calculator/recipe.html'
+    num = int(request.GET.get("servings", 1))
+    name = recipe
+    rec = DATA[recipe].copy()
+    for k, v in rec.items():
+        rec[k] = round(v * num, 2)
+    context = {
+        'name': name,
+        'recipe': rec,
+        'num': num,
+    }
+    return render(request, template_name, context)
+
--- 1.2-requests-templates/recipes/recipes/urls.py
+++ 1.2-requests-templates/recipes/recipes/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 
 from django.urls import path
 
+from calculator.views import home_view, recipes_view, recipe_view
+
 urlpatterns = [
-    # здесь зарегистрируйте вашу view-функцию
+    path('', home_view, name='home'),
+    path('recipes/', recipes_view, name='recipes'),
+    path('recipes/<str:recipe>/', recipe_view, name='recipe'),
 ]
