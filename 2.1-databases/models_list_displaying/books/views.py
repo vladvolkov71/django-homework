517e9c7f25fb437cd2bb982496e9612aaa689d9a
“Vlad <vladvolkov71@gmail.com>
2024-04-01 18:54:15 +0300

    Решение задания 2 Библиотека

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,36 @@
+from time import strftime, strptime
+
+from django.core.paginator import Paginator
 from django.shortcuts import render
+
+from books.models import Book
 
 
 def books_view(request):
     template = 'books/books_list.html'
-    context = {}
+    context = {
+        'books': Book.objects.all()
+    }
+    return render(request, template, context)
+
+
+def date_view(request, pub_date):
+    template = 'books/books_date.html'
+    data = Book.objects.filter(pub_date=pub_date)
+    data_p = Book.objects.filter(pub_date__lt=pub_date).order_by('-pub_date').first()
+    data_n = Book.objects.filter(pub_date__gt=pub_date).order_by('pub_date').first()
+    if data_p:
+        data_prev = data_p.pub_date.strftime('%Y-%m-%d')
+    else:
+        data_prev = None
+    if data_n:
+        data_next = data_n.pub_date.strftime('%Y-%m-%d')
+    else:
+        data_next = None
+
+    context = {
+        'books': data,
+        'data_prev': data_prev,
+        'data_next': data_next,
+    }
     return render(request, template, context)
