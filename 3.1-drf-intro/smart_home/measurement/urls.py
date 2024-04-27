5174be277b272c6dd8bb06acc85f6864da4023b4
“Vlad <vladvolkov71@gmail.com>
2024-04-27 20:00:47 +0300

    Решение задания 3

--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -5,7 +5,7 @@ from .views import ListSensor, UpdateDeleteSensor, InfoSensorView, AddMeasuremen
 urlpatterns = [
     # TODO: зарегистрируйте необходимые маршруты
     path('sensors/', ListSensor.as_view()),
-    path('sensors/<int:pk>/', InfoSensorView.as_view()),
+    path('sensors/<int:id>/', InfoSensorView.as_view()),
     path('sensors/<int:pk>/<str:sensor_description>/', UpdateDeleteSensor.as_view()),
     path('sensors/<str:sensor_name>/<str:sensor_description>/', ListSensor.as_view()),
     path('measurements/<int:pk>/<str:temperature>/', AddMeasurement.as_view()),
--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,4 +1,7 @@
 from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView, CreateAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .models import Sensors, Measurements
 from .serializers import SensorsSerializer, MeasurementSerializer, InfoSensorSerializer
 
@@ -27,9 +30,13 @@ class AddMeasurement(CreateAPIView):
     serializer_class = MeasurementSerializer
 
 
-class InfoSensorView(ListCreateAPIView):
+class InfoSensorView(APIView):
     """
     Информация по датчику
     """
-    queryset = Sensors.objects.all()
-    serializer_class = InfoSensorSerializer
+    # queryset = Sensors.objects.filter(id=id)
+    # serializer_class = InfoSensorSerializer
+    def get(self, request, id):
+        sensor = Sensors.objects.filter(id=id)
+        data = InfoSensorSerializer(sensor, many=True).data
+        return Response(data)
